30b842fe53738b9cdcdfcaa98d7b451d48525338
ksmin23 <sonagy23@gmail.com>
2020-10-13 15:34:15 +0000

    add redis construct

--- aws_rss_feed_trans_bot/aws_rss_feed_trans_bot_stack.py
+++ aws_rss_feed_trans_bot/aws_rss_feed_trans_bot_stack.py
@@ -10,8 +10,10 @@ from aws_cdk import (
   aws_lambda as _lambda,
   aws_logs,
   aws_events,
-  aws_events_targets
+  aws_events_targets,
+  aws_elasticache
 )
+
 
 class AwsRssFeedTransBotStack(core.Stack):
 
@@ -31,6 +33,51 @@ class AwsRssFeedTransBotStack(core.Stack):
     s3_bucket = s3.Bucket(self, 'TransRecentAnncmtBucket',
       bucket_name='aws-rss-feed-{region}-{account}'.format(region=core.Aws.REGION,
         account=core.Aws.ACCOUNT_ID))
+
+
+    sg_use_elasticache = aws_ec2.SecurityGroup(self, 'RssFeedTransBotCacheClientSG',
+      vpc=vpc,
+      allow_all_outbound=True,
+      description='security group for redis client used rss feed trans bot',
+      security_group_name='use-rss-feed-trans-bot-redis'
+    )
+    core.Tags.of(sg_use_elasticache).add('Name', 'use-rss-feed-trans-bot-redis')
+
+    sg_elasticache = aws_ec2.SecurityGroup(self, 'RssFeedTransBotCacheSG',
+      vpc=vpc,
+      allow_all_outbound=True,
+      description='security group for redis used rss feed trans bot',
+      security_group_name='rss-feed-trans-bot-redis'
+    )
+    core.Tags.of(sg_elasticache).add('Name', 'rss-feed-trans-bot-redis')
+
+    sg_elasticache.add_ingress_rule(peer=sg_use_elasticache, connection=aws_ec2.Port.tcp(6379), description='use-rss-feed-trans-bot-redis')
+
+    elasticache_subnet_group = aws_elasticache.CfnSubnetGroup(self, 'RssFeedTransBotCacheSubnetGroup',
+      description='subnet group for rss-feed-trans-bot-redis',
+      subnet_ids=vpc.select_subnets(subnet_type=aws_ec2.SubnetType.PRIVATE).subnet_ids,
+      cache_subnet_group_name='rss-feed-trans-bot-redis'
+    )
+
+    translated_feed_cache = aws_elasticache.CfnCacheCluster(self, 'RssFeedTransBotCache',
+      cache_node_type='cache.t3.small',
+      num_cache_nodes=1,
+      engine='redis',
+      engine_version='5.0.5',
+      auto_minor_version_upgrade=False,
+      cluster_name='rss-feed-trans-bot-redis',
+      snapshot_retention_limit=3,
+      snapshot_window='17:00-19:00',
+      preferred_maintenance_window='mon:19:00-mon:20:30',
+      #XXX: Do not use referece for 'cache_subnet_group_name' - https://github.com/aws/aws-cdk/issues/3098
+      cache_subnet_group_name=elasticache_subnet_group.cache_subnet_group_name, # Redis cluster goes to wrong VPC
+      #cache_subnet_group_name='rss-feed-trans-bot-redis',
+      vpc_security_group_ids=[sg_elasticache.security_group_id]
+    )
+
+    #XXX: If you're going to launch your cluster in an Amazon VPC, you need to create a subnet group before you start creating a cluster.
+    # https://docs.aws.amazon.com/AWSCloudFormation/latest/UserGuide/aws-properties-elasticache-cache-cluster.html#cfn-elasticache-cachecluster-cachesubnetgroupname
+    translated_feed_cache.add_depends_on(elasticache_subnet_group)
 
     sg_rss_feed_trans_bot = aws_ec2.SecurityGroup(self, 'RssFeedTransBotSG',
       vpc=vpc,
@@ -63,6 +110,12 @@ class AwsRssFeedTransBotStack(core.Stack):
       code=_lambda.Code.from_bucket(s3_lib_bucket, 'var/googletrans-lib.zip')
     )
 
+    redis_lib_layer = _lambda.LayerVersion(self, "RedisLib",
+      layer_version_name="redis-lib",
+      compatible_runtimes=[_lambda.Runtime.PYTHON_3_7],
+      code=_lambda.Code.from_bucket(s3_lib_bucket, "var/redis-lib.zip")
+    )
+
     lambda_fn_env = {
       'REGION_NAME': core.Aws.REGION,
       'S3_BUCKET_NAME': s3_bucket.bucket_name,
@@ -83,7 +136,7 @@ class AwsRssFeedTransBotStack(core.Stack):
       code=_lambda.Code.asset('./src/main/python/RssFeedTransBot'),
       environment=lambda_fn_env,
       timeout=core.Duration.minutes(15),
-      layers=[bs4_lib_layer, feedparser_lib_layer, googletrans_lib_layer],
+      layers=[bs4_lib_layer, feedparser_lib_layer, googletrans_lib_layer, redis_lib_layer],
       security_groups=[sg_rss_feed_trans_bot],
       vpc=vpc
     )
